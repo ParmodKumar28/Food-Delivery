128124ead53489e1931642adc8794f01958c0114
Parmod Kumar <yparmodyadav@gmail.com>
2024-05-28 17:12:18 +0530

    Cart

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,16 +1,15 @@
-import { useContext } from "react"
-import "./Cart.css"
+import { useContext } from "react";
+import "./Cart.css";
 import { StoreContext } from "../../context/StoreContext";
 
 const Cart = () => {
-
     const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
 
     return (
         <div className="cart">
             <div className="cart-items">
                 <div className="cart-items-title">
-                    <p>Items</p>
+                    <p>Image</p>
                     <p>Title</p>
                     <p>Price</p>
                     <p>Quantity</p>
@@ -21,20 +20,21 @@ const Cart = () => {
                 <hr />
                 {food_list.map((item, index) => {
                     if (cartItems[item._id] > 0) {
-                        return (<div key={index} className="cart-items-title cart-items-item">
-                            <img src={item.image} alt="" />
-                            <p>{item.name}</p>
-                            <p>{item.price}</p>
-                            <p>{cartItems[item._id]}</p>
-                            <p>{item.price*cartItems[item._id]}</p>
-                            <p>x</p>
-                        </div>
-                        )
+                        return (
+                            <div key={index} className="cart-items-row cart-items-item">
+                                <img src={item.image} alt={item.name} />
+                                <p>{item.name}</p>
+                                <p>${item.price.toFixed(2)}</p>
+                                <p>{cartItems[item._id]}</p>
+                                <p>${(item.price * cartItems[item._id]).toFixed(2)}</p>
+                                <p onClick={() => removeFromCart(item._id)} className="remove-button">x</p>
+                            </div>
+                        );
                     }
                 })}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Cart
+export default Cart;
