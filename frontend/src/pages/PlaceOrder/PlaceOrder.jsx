0e950352ad4f8c2b8a1b7b6ebaf22f3e7e315c54
Parmod Kumar <yparmodyadav@gmail.com>
2024-07-27 17:28:51 +0530

    Place order componenet created

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,6 +1,10 @@
+import { useContext } from "react";
 import "./PlaceOrder.css"
+import { StoreContext } from "../../context/StoreContext";
 
 const PlaceOrder = () => {
+
+    const {getTotalCartAmount} = useContext(StoreContext);
     return (
         <form className="place-order">
             <div className="place-order-left">
@@ -26,7 +30,29 @@ const PlaceOrder = () => {
                 <input type="text" placeholder="Phone" />
             </div>
 
-            <div className="place-order-right"></div>
+            <div className="place-order-right">
+            <div className="cart-total">
+                    <h2>Cart Total</h2>
+                    <div>
+                        <div className="cart-total-details">
+                            <p>Subtotal</p>
+                            <p>{getTotalCartAmount()}</p>
+                        </div>
+                        <hr />
+                        <div className="cart-total-details">
+                            <p>Delivery Fee</p>
+                            <p>${2}</p>
+                        </div>
+                        <hr />
+                        <div className="cart-total-details">
+                            <b>Total</b>
+                            <b>${getTotalCartAmount() + 2}</b>
+                        </div>
+                        <hr />
+                    </div>
+                    <button>PROCEED TO CHECKOUT</button>
+                </div>
+            </div>
         </form>
     )
 }
