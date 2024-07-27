65bd5b37f1adc872e7e1e7b29841e1fe89607b06
Parmod Kumar <yparmodyadav@gmail.com>
2024-07-27 18:03:06 +0530

    Database connection established

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import { connect } from 'mongoose';
+import { connectDB } from './config/db.js';
 
 
 
@@ -10,6 +12,9 @@ const port = process.env.PORT || 4000;
 // middleware
 app.use(express.json());
 app.use(cors());
+
+// db connection
+connectDB();
 
 // api routes
 app.get('/', (req, res) => res.status(200).send('API Working'));
