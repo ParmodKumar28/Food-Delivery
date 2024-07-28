309b60b2d6a8b53c4cf1cb36ab624f2e402d72be
Parmod Kumar <yparmodyadav@gmail.com>
2024-07-28 14:37:27 +0530

    Admin panel updated

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -3,17 +3,26 @@ import './List.css'
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
-function List() {
+function List({url}) {
 
-    const url = 'http://localhost:4000';
     const [list, setList] = React.useState([]);
 
     const fetchList = async () => {
         const response = await axios.get(`${url}/api/food/list`);
         console.log(response.data);
-        if(response.data.success){
+        if (response.data.success) {
             setList(response.data.data);
-        }else{
+        } else {
+            toast.error("Error");
+        }
+    }
+
+    const removeFood = async (foodId) => {
+        const response = await axios.post(`${url}/api/food/remove`, { id: foodId });
+        await fetchList();
+        if (response.data.success) {
+            toast.success(response.data.message);
+        } else {
             toast.error("Error");
         }
     }
@@ -40,7 +49,7 @@ function List() {
                         <p>{item.name}</p>
                         <p>{item.category}</p>
                         <p>{item.price}</p>
-                        <p className='cursor'>X</p>
+                        <p onClick={() => removeFood(item._id)} className='cursor'>X</p>
                     </div>
                 ))}
             </div>
