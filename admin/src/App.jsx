ea06df102d8d032f6f2ae31b0aa2d6c274c153e7
Parmod Kumar <yparmodyadav@gmail.com>
2024-07-28 12:48:24 +0530

    Nvbar and Sidebar added

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Sidebar from './components/sidebar/Sidebar'
+
 
 const App = () => {
   return (
     <div>
-      
+      <Navbar/>
+      <hr />
+      <div className="app-content">
+        <Sidebar/>
+      </div>
     </div>
   )
 }
